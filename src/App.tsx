32259a6148c664276f5c49c88532d45f6c241221
Murilo Fernandes <murilofm2010@gmail.com>
2022-03-22 21:49:45 -0300

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { ResponsiveAppBar } from "./components/AppBar";
 import logo from "./logo.svg";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
+      <Outlet />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./routes/Home";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <BrowserRouter>
+    {/* <App /> */}
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="" element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
