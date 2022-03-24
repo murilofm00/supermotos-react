b7375aad1e479411d7b8566229b6f7c4d041688e
Murilo Fernandes <murilofm2010@gmail.com>
2022-03-23 23:35:26 -0300

    Remover campo senha Usuario

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -4,7 +4,6 @@ import { Comentario } from "./Comentario";
 export class Usuario extends BaseModel {
   nome!: string;
   email!: string;
-  senha!: string;
   isAdmin!: boolean;
   comentarios?: Comentario[];
 }
